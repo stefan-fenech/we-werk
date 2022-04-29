f3438e4c3bc89351b8d7240c0fe693e05002dee8
Stefan <stefan.fenech@live.com.au>
2022-04-29 20:42:26 +1000

    added chip functionality

--- client/src/components/ClickableChip.tsx
+++ client/src/components/ClickableChip.tsx
@@ -4,12 +4,13 @@ import { useState } from 'react';
 export default function ClickableChip() {
     const [buttonAction, setButtonAction] = useState<any>({
         color: 'primary',
+        label: 'Place candidate',
         variant: 'outlined',
     });
 
     const handleClick = () => {
-        setButtonAction({ color: 'success', variant: 'filled' });
+        setButtonAction({ color: 'success', variant: 'filled', label: 'Placed!' });
     };
 
-    return <Chip label='Place candidate' size='small' variant={buttonAction.variant} color={buttonAction.color} onClick={handleClick} />;
+    return <Chip label={buttonAction.label} size='small' variant={buttonAction.variant} color={buttonAction.color} onClick={handleClick} />;
 }
